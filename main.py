fc9025cf4ebe48343e9908687aac726a23e758f5
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2024-10-07 17:20:49 +0700

    Fix data type.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-
 def main():
     while True:
         logo_print()
         packaging_name = input("Enter the packaging name: ")
         type_of_coffee_beans = input("Enter the type of coffee beans: ")
-        weight_of_coffee_beans = input("Enter the weight (in grams): ")
-        price_of_coffee_beans = input("Enter the coffee beans price per gram ($): ")
+        weight_of_coffee_beans = float(input("Enter the weight (in grams): "))  # Convert to float
+        price_of_coffee_beans = float(input("Enter the coffee beans price per gram ($): "))  # Convert to float
         packaging_tone = input("Enter the packaging tone: ")
-        price_of_packaging_tone = input("Enter the packaging price ($) : ")
-        selling_price = input("Enter the selling price ($) : ")
+        price_of_packaging_tone = float(input("Enter the packaging price ($): "))  # Convert to float
+        selling_price = float(input("Enter the selling price ($): "))  # Convert to float
         confirmation = input("Confirm? (Y): ")
 
         if confirmation == 'Y':
@@ -24,16 +23,18 @@ def output(packaging_name, type_of_coffee_beans, weight_of_coffee_beans, price_o
     infinity_void(5)
     print("--- Coffee Packaging Details --- \n")
     print("Packaging Name: " + packaging_name)
-    print("Packaging Tone: " +packaging_tone)
-    print("Packaging Price: $" + price_of_packaging_tone)
-    print("Packaging Selling Price: $" + selling_price +"\n")
-    print(f"{type_of_coffee_beans} | {weight_of_coffee_beans}g | ${price_of_coffee_beans}g \n")
+    print("Packaging Tone: " + packaging_tone)
+    print("Packaging Price: $" + str(price_of_packaging_tone))
+    print("Packaging Selling Price: $" + str(selling_price) + "\n")
+    print(f"{type_of_coffee_beans} | {weight_of_coffee_beans}g | ${price_of_coffee_beans}/g \n")
 
-    profit_loss_value = selling_price - ((weight_of_coffee_beans*price_of_coffee_beans) + price_of_packaging_tone)
-    print("Profit/Loss Value: $" + profit_loss_value)
+    # Calculate profit/loss
+    profit_loss_value = selling_price - ((weight_of_coffee_beans * price_of_coffee_beans) + price_of_packaging_tone)
+    print("Profit/Loss Value: $" + str(profit_loss_value))
 
-    profit_loss_percentage = profit_loss_value/selling_price*100
-    print("Profit/Loss Percentage: " + profit_loss_percentage + "%")
+    # Calculate profit/loss percentage
+    profit_loss_percentage = (profit_loss_value / selling_price) * 100
+    print("Profit/Loss Percentage: " + str(profit_loss_percentage) + "%")
 
 
 def logo_print():
@@ -47,9 +48,9 @@ def logo_print():
     """)
 
 def infinity_void(x):
-    i = 0
     for i in range(x):
         print("")
+
 
 if __name__ == "__main__":
     main()
